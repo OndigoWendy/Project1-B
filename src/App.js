bc5c650346898bf086ef031a68e0d29027dfc3db
BrianGoga21 <121162940+BrianGoga21@users.noreply.github.com>
2023-10-11 20:26:33 +0300

    Project1 Revision

--- src/App.js
+++ src/App.js
@@ -1,40 +1,14 @@
-import Wendy_Profile from './Wendy_Profile.jpg';
 import './App.css';
 import { Navbar } from './components/Navbar';
+import { Home } from './components/Home';
+
 
 function App() {
   return (
     <div className="App">
         <Navbar />
-      <header className="App-header">
-        <h1>My Portfolio</h1>
-        <br></br>
-        <img src={Wendy_Profile} alt="logo" className='logo'/>
-        <p> My name is <b>Wendy Ondigo.</b>
-          <br></br>
-          I am a fun senior web developer at microsoft. I recently graduated from KU and Moringa School and have taken up a role of mentoring young software developers.
-          To keep myself alive, I participate in our local church choir and occassional walks at Karura forest.
-          <br></br>
-          Let's connect!
-        </p>
-        <br></br>
-
-        <a
-          className="App-link"
-          href="https://github.com/OndigoWendy"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Here is my Github Profile.
-        </a>
-        <ol>
-        <li>Project 1</li>
-        <li>Project 2</li>
-        <li>Project 3</li>
-      </ol>
-      </header>
+        <Home />
       
-      <ul></ul>
     </div>
   );
 }
--- src/components/Navbar/index.js
+++ src/components/Navbar/index.js
@@ -4,14 +4,16 @@ export const Navbar = () => {
   return (
     <nav>
       <div className="navbar">
-        <div className="left">
+        <div className="left-allignment">
           <ul>
             <li><a href="/">Home</a> </li>
             <li><a href="/about">About</a> </li>
+            <li><a href="/projects">Projects</a> </li>
+            <li><a href="/skills">Skills</a> </li>
             <li><a href="/contact">Contact</a> </li>
           </ul>
         </div>
-        <div className="right">More</div>
+        <div className="right-allignment">More</div>
       </div>
     </nav>
   );
